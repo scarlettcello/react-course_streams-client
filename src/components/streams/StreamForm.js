ad99a17c01f3a1113a70796809599ae9c558d623
Carol Lee <CLee@thrillworks.com>
2020-09-25 23:33:04 -0400

    Added CRUD of streaming videos

---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class StreamForm extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      )
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => { // === (formProps), <input {...formProps.input} />
+    const className = `field ${meta.error && meta.touched ? 'error': ''}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />  
+        {this.renderError(meta)}
+      </div>
+    );
+  }
+
+  onSubmit = (formValues) => {
+    this.props.onSubmit(formValues);
+  }
+
+  render() {
+    return (
+      <form 
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field 
+          name="title"
+          label="Enter Title"
+          component={this.renderInput}
+        />
+        <Field
+          name="description"
+          label="Enter Description"
+          component={this.renderInput}
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    )
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {}
+
+  if (!formValues.title) {
+    errors.title = 'Title is required.';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'Description is required.';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  form: 'streamForm',
+  validate // === validate: validate
+})(StreamForm);
